3113ec89d64bca206bff855b3a654d964e6ad362
ArchiFleKs <lefevre.kevin@gmail.com>
2019-02-01 13:53:28 +0100

    feat(addons): add fluentd-cloudwatch

---
+++ terraform/modules/eks-addons/fluentd-cloudwatch.tf
@@ -0,0 +1,58 @@
+locals {
+  values_fluentd_cloudwatch = <<VALUES
+image:
+  tag: ${var.fluentd_cloudwatch["version"]}
+rbac:
+  create: true
+nodeSelector:
+  node-role.kubernetes.io/node: ""
+tolerations:
+  - operator: Exists
+awsRegion: "${var.aws["region"]}"
+logGroupName: "${var.fluentd_cloudwatch["log_group_name"]}"
+extraVars:
+  - "{ name: FLUENT_UID, value: '0' }"
+updateStrategy:
+  type: RollingUpdate
+VALUES
+  values_fluentd_cloudwatch_kiam = <<VALUES
+image:
+  tag: ${var.fluentd_cloudwatch["version"]}
+rbac:
+  create: true
+nodeSelector:
+  node-role.kubernetes.io/node: ""
+tolerations:
+  - operator: Exists
+awsRole: "${join(",", data.terraform_remote_state.eks.*.fluentd-cloudwatch-kiam-role-arn[0])}"
+awsRegion: "${var.aws["region"]}"
+logGroupName: "${var.fluentd_cloudwatch["log_group_name"]}"
+extraVars:
+  - "{ name: FLUENT_UID, value: '0' }"
+updateStrategy: 
+  type: RollingUpdate
+VALUES
+}
+
+resource "kubernetes_namespace" "fluentd_cloudwatch" {
+  count = "${var.fluentd_cloudwatch["enabled"] ? 1 : 0 }"
+  metadata {
+    annotations {
+      "iam.amazonaws.com/permitted" = ".*"
+    }
+
+    name = "${var.fluentd_cloudwatch["namespace"]}"
+  }
+}
+
+resource "helm_release" "fluentd_cloudwatch" {
+  depends_on = [
+    "kubernetes_namespace.fluentd_cloudwatch"
+  ]
+  count     = "${var.fluentd_cloudwatch["enabled"] ? 1 : 0 }"
+  name      = "fluentd-cloudwatch"
+  chart     = "incubator/fluentd-cloudwatch"
+  version   = "${var.fluentd_cloudwatch["chart_version"]}"
+  values    = ["${concat(list(var.fluentd_cloudwatch["use_kiam"] ? local.values_fluentd_cloudwatch_kiam : local.values_fluentd_cloudwatch),list(var.fluentd_cloudwatch["extra_values"]))}"]
+  namespace = "${var.fluentd_cloudwatch["namespace"]}"
+}
---
+++ terraform/modules/eks/eks-fluentd-cloudwatch.tf
@@ -0,0 +1,52 @@
+//
+// [fluentd-cloudwatch]
+//
+resource "aws_iam_policy" "eks-fluentd-cloudwatch" {
+  count  = "${var.fluentd_cloudwatch["create_iam_resources"] ? 1 : var.fluentd_cloudwatch["create_iam_resources_kiam"] ? 1 : 0 }"
+  name   = "terraform-eks-${var.cluster-name}-fluentd-cloudwatch"
+  policy = "${var.fluentd_cloudwatch["iam_policy"]}"
+}
+
+resource "aws_iam_role_policy_attachment" "eks-fluentd-cloudwatch" {
+  count      = "${var.fluentd_cloudwatch["create_iam_resources"] ? length(var.node-pools) : 0 }"
+  role       = "${aws_iam_role.eks-node.*.name[count.index]}"
+  policy_arn = "${aws_iam_policy.eks-fluentd-cloudwatch.arn}"
+}
+
+resource "aws_iam_role" "eks-fluentd-cloudwatch-kiam" {
+  name  = "terraform-eks-${var.cluster-name}-fluentd-cloudwatch-kiam"
+  count = "${var.fluentd_cloudwatch["create_iam_resources_kiam"] ? 1 : 0 }"
+
+  assume_role_policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "ec2.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    },
+    {
+      "Sid": "",
+      "Effect": "Allow",
+      "Principal": {
+        "AWS": "${aws_iam_role.eks-kiam-server-role.*.arn[count.index]}"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+POLICY
+}
+
+resource "aws_iam_role_policy_attachment" "eks-fluentd-cloudwatch-kiam" {
+  count      = "${var.fluentd_cloudwatch["create_iam_resources_kiam"] ? 1 : 0 }"
+  role       = "${aws_iam_role.eks-fluentd-cloudwatch-kiam.*.name[count.index]}"
+  policy_arn = "${aws_iam_policy.eks-fluentd-cloudwatch.*.arn[count.index]}"
+}
+
+output "fluentd-cloudwatch-kiam-role-arn" {
+  value = "${aws_iam_role.eks-fluentd-cloudwatch-kiam.*.arn}"
+}
