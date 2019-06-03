0d159fd857a43019e6aa2564fc960297c51cb339
ArchiFleKs <lefevre.kevin@gmail.com>
2019-06-03 16:49:47 +0200

    feat: support nginx-ingress l7 TLS termination

--- terraform/modules/eks-addons/nginx-ingress.tf
+++ terraform/modules/eks-addons/nginx-ingress.tf
@@ -1,5 +1,5 @@
 locals {
-  values_nginx_ingress = <<VALUES
+  values_nginx_ingress_l4 = <<VALUES
 controller:
   kind: "DaemonSet"
   daemonset:
@@ -31,6 +31,35 @@ controller:
 defaultBackend:
   replicaCount: 2
 VALUES
+  values_nginx_ingress_l7 = <<VALUES
+controller:
+  kind: "DaemonSet"
+  daemonset:
+    useHostPort: true
+  service:
+    targetPorts:
+      http: http
+      https: http
+    annotations:
+      # replace with the correct value of the generated certificate in the AWS console
+      # the backend instances are HTTP
+      service.beta.kubernetes.io/aws-load-balancer-backend-protocol: "http"
+      # Map port 443
+      service.beta.kubernetes.io/aws-load-balancer-ssl-ports: "https"
+      # Ensure the ELB idle timeout is less than nginx keep-alive timeout. By default,
+      # NGINX keep-alive is set to 75s. If using WebSockets, the value will need to be
+      # increased to '3600' to avoid any potential issues.
+      service.beta.kubernetes.io/aws-load-balancer-connection-idle-timeout: "3600"
+    externalTrafficPolicy: "Cluster"
+  publishService:
+    enabled: true
+  config:
+    use-proxy-protocol: "false"
+    use-forwarded-headers: "true"
+    proxy-real-ip-cidr: "0.0.0.0/0"
+defaultBackend:
+  replicaCount: 2
+VALUES
 }
 
 resource "helm_release" "nginx_ingress" {
@@ -38,6 +67,6 @@ resource "helm_release" "nginx_ingress" {
   name      = "nginx-ingress"
   chart     = "stable/nginx-ingress"
   version   = "${var.nginx_ingress["chart_version"]}"
-  values    = ["${concat(list(var.nginx_ingress["use_nlb"] ? local.values_nginx_ingress_nlb : local.values_nginx_ingress),list(var.nginx_ingress["extra_values"]))}"]
+  values    = ["${concat(list(var.nginx_ingress["use_nlb"] ? local.values_nginx_ingress_nlb : var.nginx_ingress["use_l7"] ? local.values_nginx_ingress_l7 : local.values_nginx_ingress_l4),list(var.nginx_ingress["extra_values"]))}"]
   namespace = "${var.nginx_ingress["namespace"]}"
 }
