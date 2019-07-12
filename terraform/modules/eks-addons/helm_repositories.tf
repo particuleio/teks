004fae9248e0e70f081859a9b7e1b0b725dfa850
ArchiFleKs <lefevre.kevin@gmail.com>
2019-07-12 14:27:30 +0200

    fix: weave flux 404

--- terraform/modules/eks-addons/helm_repositories.tf
+++ terraform/modules/eks-addons/helm_repositories.tf
@@ -1,6 +1,6 @@
 data "helm_repository" "flux" {
   name = "flux"
-  url  = "https://weaveworks.github.io/flux"
+  url  = "https://raw.githubusercontent.com/fluxcd/flux/gh-pages/"
 }
 
 data "helm_repository" "stable" {
