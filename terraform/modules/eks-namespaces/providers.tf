6bca3fd5bd929f872c4f1e9aaa9e9394b0b885c2
Kevin Lefevre <lefevre.kevin@gmail.com>
2019-11-04 16:12:17 +0100

    fix: remove remote state from eks-namespaces
    Signed-off-by: Kevin Lefevre <lefevre.kevin@gmail.com>

--- terraform/modules/eks-namespaces/providers.tf
+++ terraform/modules/eks-namespaces/providers.tf
@@ -9,14 +9,3 @@ terraform {
 provider "kubernetes" {
   config_path = var.eks["kubeconfig_path"]
 }
-
-data "terraform_remote_state" "eks" {
-  backend = "s3"
-
-  config = {
-    bucket = var.eks["remote_state_bucket"]
-    key    = var.eks["remote_state_key"]
-    region = var.aws["region"]
-  }
-}
-
