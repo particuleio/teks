6572f10c53c2cc68d6eac6663d2d2b38daa17682
Kevin Lefevre <lefevre.kevin@gmail.com>
2020-04-06 13:10:22 +0200

    fix: revert provider fix as it is fixed upstream
    Signed-off-by: Kevin Lefevre <lefevre.kevin@gmail.com>

--- terraform/live/demo/eu-west-3/eks/providers.tf
+++ terraform/live/demo/eu-west-3/eks/providers.tf
@@ -20,7 +20,6 @@ provider "kubernetes" {
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
   token                  = data.aws_eks_cluster_auth.cluster.token
   load_config_file       = false
-  version                = "1.10"
 }
 
 data "aws_region" "current" {}
