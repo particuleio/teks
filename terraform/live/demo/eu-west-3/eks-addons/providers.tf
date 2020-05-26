6471ce55cbe2f774f1ffd05e8d0c08c92c55340f
Kevin Lefevre <lefevre.kevin@gmail.com>
2020-05-26 23:00:51 +0200

    fix: remove unused datasources
    Signed-off-by: Kevin Lefevre <lefevre.kevin@gmail.com>

--- terraform/live/demo/eu-west-3/eks-addons/providers.tf
+++ terraform/live/demo/eu-west-3/eks-addons/providers.tf
@@ -33,12 +33,6 @@ provider "helm" {
   }
 }
 
-data "aws_availability_zones" "available" {
-}
-
-data "aws_caller_identity" "current" {
-}
-
 data "aws_eks_cluster" "cluster" {
   name = var.cluster-name
 }
