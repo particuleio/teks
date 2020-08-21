6f2c0a73352a591aa68be6be31c70c65cff39113
Kevin Lefevre <lefevre.kevin@gmail.com>
2020-08-21 17:01:24 +0200

    fix: provider config conflict with upstream module
    Signed-off-by: Kevin Lefevre <lefevre.kevin@gmail.com>

---
+++ terraform/live/demo/eu-west-3/eks/versions.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_version = ">= 0.12.9"
+
+  required_providers {
+    aws        = ">= 2.55.0"
+    local      = ">= 1.4"
+    null       = ">= 2.1"
+    template   = ">= 2.1"
+    random     = ">= 2.1"
+    kubernetes = ">= 1.11.1"
+    kubectl = {
+      source  = "gavinbunney/kubectl"
+      version = "1.6.2"
+    }
+  }
+}
