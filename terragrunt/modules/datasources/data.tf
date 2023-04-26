fbe2bb6348c4662aec86d6b425dc3bf92921fcf1
Kevin Lefevre <kevin@particule.io>
2023-04-26 10:33:56 +0200

    rebase
    Signed-off-by: Kevin Lefevre <kevin@particule.io>

---
+++ terragrunt/modules/datasources/data.tf
@@ -0,0 +1,8 @@
+data "aws_region" "current" {}
+
+data "aws_availability_zones" "available" {}
+
+data "aws_partition" "current" {}
+
+data "aws_caller_identity" "current" {}
+
---
+++ terragrunt/modules/datasources/main.tf
@@ -0,0 +1,6 @@
+terraform {
+  required_version = ">= 1.0"
+  required_providers {
+    aws = ">= 3.72"
+  }
+}
